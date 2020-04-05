eed1f81f1ca9b6c6dd2fbc17107211ed6d228d8c
Fede Otaran <otaran.federico@gmail.com>
2020-04-05 18:32:11 -0300

    Initial commit. :muscle:

---
+++ lib/gsx.ex
@@ -0,0 +1,18 @@
+defmodule Gsx do
+  @moduledoc """
+  Documentation for `Gsx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gsx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gsx_test.exs
@@ -0,0 +1,8 @@
+defmodule GsxTest do
+  use ExUnit.Case
+  doctest Gsx
+
+  test "greets the world" do
+    assert Gsx.hello() == :world
+  end
+end
